4c0bc17a3b5ce967ac8b8bde89e3aa457c843293
B1lkao <hrabousolesia@gmail.com>
2022-11-13 17:00:13 +0200

    Виконала друге завдання

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,31 @@
+import throttle from 'lodash.throttle'
+
+import Player from '@vimeo/player'
+
+const iframe = document.querySelector('iframe');
+
+const player = new Player(iframe)
+
+ player.on('play', function() {
+        console.log('played the video!');
+    });
+
+player.getVideoTitle().then(function (title) {
+    console.log('title:', title);
+});
+
+player.on('timeupdate', throttle(function (data) {
+    localStorage.setItem("videoplayer-current-time", data.seconds);
+},1000));
+
+player.setCurrentTime(localStorage.getItem("videoplayer-current-time"))
+    .then(function (seconds) {
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            break;
+        default:
+            break;
+    }
+});
 
