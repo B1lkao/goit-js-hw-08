25d1710740d49b0300b00bb744ab5b2652b5928c
B1lkao <hrabousolesia@gmail.com>
2022-11-13 16:54:15 +0200

    Виконала перше завдання

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,17 @@
-// Add imports above this line
 import { galleryItems } from './gallery-items';
-// Change code below this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+import cardTemp from "../template/cards.hbs"
 
-console.log(galleryItems);
+const galaryDiv = document.querySelector('.gallery');
+
+const createGalery = collection => 
+    galaryDiv.insertAdjacentHTML('afterbegin', collection.map(cardTemp).join(''));
+    
+createGalery(galleryItems)
+const lightbox = new SimpleLightbox('.gallery a', {captionsData:'alt',captionDelay:250});
+const onImgClick = event => {
+    event.preventDefault();
+}
+ 
+galaryDiv.addEventListener('click', onImgClick);
