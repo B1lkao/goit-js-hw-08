295c46bc9f5cc509a54be8df2b2e08adaf2701f1
B1lkao <hrabousolesia@gmail.com>
2022-11-22 18:15:18 +0200

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -7,11 +7,11 @@ const LOCAL_DATA_NAME = "feedback-form-state";
 
 const currentLocal = JSON.parse(localStorage.getItem(LOCAL_DATA_NAME));
 
-const localPrint = {};
+// const localPrint = {};
 
 const onFormInput = event => {
     localPrint[event.target.name] = event.target.value;
-    // localStorage.setItem(LOCAL_DATA_NAME, JSON.stringify(localPrint));
+    localStorage.setItem(LOCAL_DATA_NAME, JSON.stringify(localPrint));
  
 };
 form.addEventListener('input', throttle(onFormInput, 500));
